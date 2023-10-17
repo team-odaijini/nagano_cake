98db3e5b4b54bc427d392ca5326df62c63130df4
ikehiro1 <136701716+ikehiro1@users.noreply.github.com>
2023-10-17 05:10:06 +0000

    ルート設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,63 +1,39 @@
 Rails.application.routes.draw do
 
   namespace :admin do
-    get 'customers/index'
-    get 'customers/show'
-    get 'customers/edit'
-    get 'customers/update'
+    resources :customers, only: [:index, :show, :edit, :update]
   end
   namespace :admin do
-    get 'genres/index'
-    get 'genres/create'
-    get 'genres/edit'
-    get 'genres/update'
+   resources :genres, only: [:index, :create, :edit, :update]
   end
   namespace :admin do
-    get 'items/index'
-    get 'items/new'
-    get 'items/create'
-    get 'items/show'
-    get 'items/edit'
-    get 'items/update'
+   resources :items, only: [:new, :create, :show, :edit, :update]
   end
   namespace :admin do
     get 'homes/top'
   end
   namespace :public do
-    get 'addresses/index'
-    get 'addresses/edit'
-    get 'addresses/create'
-    get 'addresses/update'
-    get 'addresses/destroy'
+   resources :addresses, only: [:index, :edit, :create, :update, :destroy]
   end
   namespace :public do
-    get 'orders/new'
+    resources :orders, only: [:new, :create, :index, :show]
     get 'orders/confirm'
     get 'orders/thanks'
-    get 'orders/create'
-    get 'orders/index'
-    get 'orders/show'
   end
   namespace :public do
-    get 'cart_items/index'
-    get 'cart_items/update'
-    get 'cart_items/destroy'
+    resources :cart_items, only: [:index, :update, :destroy, :create]
     get 'cart_items/destroy_all'
-    get 'cart_items/create'
   end
   namespace :public do
-    get 'customers/show'
-    get 'customers/edit'
-    get 'customers/update'
-    get 'customers/leave'
-    get 'customers/withdraw'
+   resources :customers, only: [:show, :edit, :update] 
+   get 'customers/leave'
+   get 'customers/withdraw'
   end
   namespace :public do
-    get 'items/index'
-    get 'items/show'
+   resources :items, only: [:index, :show]
   end
   namespace :public do
-    get 'homes/top'
+    root :to => 'homes#top'
     get 'homes/about'
   end
   devise_for :customers, skip: [:passwords], controllers: {
