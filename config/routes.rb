68eccb5d3a6d82f16d31f6e3ea46a76dbaa69e15
ikehiro1 <136701716+ikehiro1@users.noreply.github.com>
2023-10-17 06:03:32 +0000

    ルート修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,38 +1,27 @@
 Rails.application.routes.draw do
 
   namespace :admin do
-    resources :customers, only: [:index, :show, :edit, :update]
-  end
-  namespace :admin do
+   resources :customers, only: [:index, :show, :edit, :update]
    resources :genres, only: [:index, :create, :edit, :update]
-  end
-  namespace :admin do
    resources :items, only: [:new, :create, :show, :edit, :update]
-  end
-  namespace :admin do
     get 'homes/top'
   end
-  namespace :public do
+  
+  scope module: :public do
    resources :addresses, only: [:index, :edit, :create, :update, :destroy]
-  end
-  namespace :public do
-    resources :orders, only: [:new, :create, :index, :show]
+   resources :orders, only: [:new, :create, :index, :show]
     get 'orders/confirm'
     get 'orders/thanks'
-  end
-  namespace :public do
-    resources :cart_items, only: [:index, :update, :destroy, :create]
+  
+   resources :cart_items, only: [:index, :update, :destroy, :create]
     get 'cart_items/destroy_all'
-  end
-  namespace :public do
+ 
    resources :customers, only: [:show, :edit, :update] 
-   get 'customers/leave'
-   get 'customers/withdraw'
-  end
-  namespace :public do
+    get 'customers/leave'
+    get 'customers/withdraw'
+  
    resources :items, only: [:index, :show]
-  end
-  namespace :public do
+ 
     root :to => 'homes#top'
     get 'homes/about'
   end
