efd0f9b66ddf0a39f53ec1236897c166a04a9f99
EC2 Default User <143916229+daisuke8331@users.noreply.github.com>
2023-10-17 11:08:07 +0000

    商品登録ページ作成

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -3,9 +3,14 @@ class Admin::ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
+    @genres = Genre.all
   end
 
   def create
+    @item = Item.new(item_params)
+    @item.save
+    redirect_to admin_item_path(@item.id) #新規登録した情報を持って詳細ページに飛ぶ
   end
 
   def show
@@ -16,4 +21,11 @@ class Admin::ItemsController < ApplicationController
 
   def update
   end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:genre_id, :name, :explanation, :excluding_tax, :is_sale_status, :image)
+  end
+
 end
