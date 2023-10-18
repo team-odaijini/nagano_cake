1b5c25d29cf803c2422d830330e310432b12f36e
EC2 Default User <143916229+daisuke8331@users.noreply.github.com>
2023-10-18 05:25:10 +0000

    商品詳細画面作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,19 @@ class Item < ApplicationRecord
      has_many :order_details
      has_many :cart_items
      belongs_to :genre
-     has_one_attached :image 
-     
+     has_one_attached :image
+
+     def get_image
+       unless image.attached?
+         file_path = Rails.root.join('app/assets/images/no_image.jpg')
+         image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+       end
+       image
+     end
+
+     #消費税率を計算するアクション
+     def add_tax_excluding_tax
+       (self.excluding_tax * 1.10).round
+     end
+
 end
