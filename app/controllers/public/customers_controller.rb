7325a9d8a7f72703625587dc65cba5335c779a6b
ikehiro1 <136701716+ikehiro1@users.noreply.github.com>
2023-10-20 07:41:47 +0000

    会員側　退会確認、注文履歴一部実装

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,18 +1,26 @@
 class Public::CustomersController < ApplicationController
   def show
-  #  @customer = Customer.find(params[:id])
+  　　  @customer = current_customer
   end
 
   def edit
+      @customer = current_customer
   end
 
   def update
+      @customer = current_customer
+      @customer.update(customer_params)
+      redirect_to customer_path
   end
 
   def leave
   end
 
   def withdraw
+      @customer = current_customer
+      @customer.update
+      reset_session
+      redirect_to root_path
   end
   
  private
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,7 @@
 class Order < ApplicationRecord
     has_many :order_details
     belongs_to :customer
+    
+    enum price_all: {クレジット:0, 銀行振込:1}
+    enum status: {入金待ち:0, 入金確認:1, 制作中:2, 発送準備中:3, 発送済み:4}
 end
