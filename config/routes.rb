b78043c97e3ab49fc7252f8f67bc0c0b7951cb94
EC2 Default User <143916229+daisuke8331@users.noreply.github.com>
2023-10-21 09:23:26 +0000

    顧客商品ページ作成中

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
 
+  devise_for :customers, skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
+
+  devise_for :admin, skip: [:registraions, :passwords], controllers: {
+    sessions: "admin/sessions"
+  }
+
+
   namespace :admin do
     get 'order_details/update'
   end
@@ -32,15 +42,6 @@ Rails.application.routes.draw do
     root :to => 'homes#top'
     get 'homes/about'
   end
-  devise_for :customers, skip: [:passwords], controllers: {
-    registrations: "public/registrations",
-    sessions: 'public/sessions'
-  }
 
-  devise_for :admin, skip: [:registraions, :passwords], controllers: {
-    sessions: "admin/sessions"
-  }
-
-  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
