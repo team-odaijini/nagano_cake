0981c5b352b7576c57fdf0a2fb6e12d1fb696ded
ikehiro1 <136701716+ikehiro1@users.noreply.github.com>
2023-10-22 06:40:42 +0000

    カート機能

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,16 +1,32 @@
 class Public::CartItemsController < ApplicationController
+  
   def index
+    @cart_items = current_customer.cart_items
   end
 
   def update
+     @cart_item.update(quantity: params[:quantity].to_i)
+     redirect_to request.referer
   end
 
   def destroy
+    @cart_item = CartItem.find(params[:id])
+    @cart_item.destroy
+    redirect_to request.referer
   end
 
   def destroy_all
+    @cart_item.destroy
+     redirect_to request.referer
   end
 
   def create
+    @cart_item = current_customer.cart_items.new(item_id: params[:item_id])
+    @cart_item.quantity += params[:quantity].to_i
+    if @cart_item.save
+      redirect_to request.referer
+    else
+      redirect_to items_path
+    end  
   end
 end
--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,4 +1,8 @@
 class CartItem < ApplicationRecord
     belongs_to :customer
     belongs_to :item
+    
+    #def add_tax_excluding_tax
+    #   (self.excluding_tax * 1.10).round
+    #end
 end
