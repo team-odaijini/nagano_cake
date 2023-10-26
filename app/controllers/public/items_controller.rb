2cb761c5bfb8ea53c07eb440752001f19e5c593c
EC2 Default User <143916229+daisuke8331@users.noreply.github.com>
2023-10-26 04:53:39 +0000

    ページネーション実装

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,6 +1,7 @@
 class Public::ItemsController < ApplicationController
   def index
-    @items = Item.where(is_sale_status:true)#is_sale_statusがtrue=販売中の商品のみ一覧で並べる
+    @items = Item.page(params[:page]).where(is_sale_status:true)#is_sale_statusがtrue=販売中の商品のみ一覧で並べる
+    @item = Item.where(is_sale_status:true)
   end
 
   def show
