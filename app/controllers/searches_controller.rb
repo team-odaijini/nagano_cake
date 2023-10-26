bafcdca1a7bbf462f6c33fcdd8c0f71320531694
ikehiro1 <136701716+ikehiro1@users.noreply.github.com>
2023-10-26 08:17:46 +0000

    ジャンル検索・退会画面デザイン一部変更

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -3,5 +3,6 @@ class SearchesController < ApplicationController
   def genre_search
     @genre_id = params[:genre_id]
     @items = Item.where(genre_id: @genre_id)
+    @genres = Genre.all
   end
 end
