98c155fcf536acc96ce79462a613228d888b2f67
ikehiro1 <136701716+ikehiro1@users.noreply.github.com>
2023-10-27 10:29:00 +0000

    ログイン、ログアウト、新規登録の遷移先変更

--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -59,4 +59,8 @@ class Public::RegistrationsController < Devise::RegistrationsController
   # def after_inactive_sign_up_path_for(resource)
   #   super(resource)
   # end
+  
+  def after_sign_up_path_for(resource_or_scope)
+   customer_path(customer_session_path)
+  end
 end
